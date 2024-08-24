bd06aa030c222890c02b2655ec5553a4f743dda6
Globox1997 <mitschke11@web.de>
2024-08-24 15:35:21 +0200

    first commit

---
+++ src/main/java/net/oblivion/OblivionClient.java
@@ -0,0 +1,14 @@
+package net.oblivion;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class OblivionClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
