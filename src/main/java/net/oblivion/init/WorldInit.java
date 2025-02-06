0b8376030b525a433a76ae7ff121e0d35dad8b82
Globox1997 <mitschke11@web.de>
2025-02-06 11:57:25 +0100

    flush

--- src/main/java/net/oblivion/init/WorldInit.java
+++ src/main/java/net/oblivion/init/WorldInit.java
@@ -1,13 +1,21 @@
 package net.oblivion.init;
 
 import com.mojang.serialization.MapCodec;
+import net.minecraft.client.gui.hud.InGameOverlayRenderer;
+import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.world.World;
 import net.minecraft.world.gen.foliage.FoliagePlacer;
 import net.minecraft.world.gen.foliage.FoliagePlacerType;
+import net.oblivion.OblivionMain;
 import net.oblivion.world.*;
 
 public class WorldInit {
+
+    public static final RegistryKey<World> OBLIVION_WORLD = RegistryKey.of(RegistryKeys.WORLD, OblivionMain.identifierOf("oblivion"));
 
     public static final FoliagePlacerType<SlimTreeFoliagePlacer> SLIM_TREE_FOLIAGE_PLACER = register("slim_tree_foliage_placer", SlimTreeFoliagePlacer.CODEC);
     public static final FoliagePlacerType<IronTreeFoliagePlacer> IRON_TREE_FOLIAGE_PLACER = register("iron_tree_foliage_placer", IronTreeFoliagePlacer.CODEC);
@@ -17,6 +25,11 @@ public class WorldInit {
         return Registry.register(Registries.FOLIAGE_PLACER_TYPE, id, new FoliagePlacerType<>(codec));
     }
 
+    // Dimension
+    // Todo: Mob drops
+    // Todo: Mob Sounds
+    // Guidelight teleport
+    // Set wood toolRequired and mixin into item isCorrectForDrops, maybe requires earlystage compat
     public static void init() {
     }
 }
